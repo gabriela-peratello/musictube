1dc93518fb87525dd85259634ed293f2f59629e9
gabriela peratello <gabriela.p.silva10@aluno.senai.br>
2026-02-20 11:28:49 -0300

    arrumando

--- model/musica.py
+++ model/musica.py
@@ -18,3 +18,20 @@ def recuperar_musicas():
 
 def salvar_musica(cantor:str, nome_musica:str, duracao:str, url_imagem:str, genero:str) -> bool:
     """ Essa função tem como intuito de adicionar uma música."""
+
+    try:
+        conexao, cursor = conectar()
+        # Executar o insert
+        cursor.execute("""
+                    INSERT INTO musica (cantor, nome, duracao, url_capa, genero) 
+                    VALUES (%s, %s, %s, %s, %s);
+                    """,
+                    (cantor, nome_musica, duracao, url_imagem, genero )
+                    )
+        # Confirmando o insert
+        conexao.commit()
+        conexao.close()
+
+        return True
+    except:
+        return False
---
+++ teste.py
@@ -0,0 +1,3 @@
+from model.musica import salvar_musica
+
+salvar_musica("Charlie Puth", "We don't talk anymore", "00:03:51", "https://i1.sndcdn.com/artworks-000642578785-bsan65-t500x500.jpg", "Pop")
